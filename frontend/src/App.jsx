02e511af23b7b5363f6d3964b66a35c94bb9076e
ankeshkashyap <ankeshkashyap2222@gmail.com>
2026-08-01 05:13:55 +0530

    Initial project setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,10 @@
+export default function App() 
+{
+  return (
+    <div classname="min-h-screen flex items-center justify-center bg-slate-100"> 
+    <h1 classname="text-5xl font-bold text-blue-600">
+      SMART RESUME ATS ANALYZER
+    </h1>
+    </div>
+  );
+}
