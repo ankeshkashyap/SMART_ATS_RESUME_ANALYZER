9efbda04917ebe764a8afbd691a20780c06b7ed3
ankeshkashyap <ankeshkashyap2222@gmail.com>
2026-08-03 16:33:39 +0530

    feat(ui): complete dashboard interface

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -37,7 +37,7 @@ useEffect(()=>{
     return (<div>
             <DashboardLayout user={user}>
                 <DashboadHeader user={user}/>
-            <div className="grid grid-cols-4 gap-6 mt-8">
+            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6 mt-8">
                 <StatCard
                     title="Resumes Analyzed"
                     value="12"
